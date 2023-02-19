d78e409f777fc3c56e4beaf731b7af6e0399c223
prabhalagayathri <118177929+prabhalagayathri@users.noreply.github.com>
2023-02-19 10:57:55 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "This is Gayathri\n";
+  cout << "This is Gayathri\n"; 
   cout << "I have successfully built and run\n";
   return 0;
 }
